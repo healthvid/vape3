79be5ecde28ff6cc9e9edc0f724baf95f6afb387
Tingting-Jiang <jtting@126.com>
2023-01-02 19:28:54 -0800

    adjust styles

--- src/components/commentsTurnedOff.js
+++ src/components/commentsTurnedOff.js
@@ -2,7 +2,7 @@ import React from "react";
 const CommentsTurnedOff = ()=>{
     return (
         <div className="center" id='center'>
-        <div className='fs-2 border border-3 border-secondary d-flex justify-content-center comments-border'>Comments are turned off.</div>
+        <div className='fs-3 border border-3 border-secondary d-flex justify-content-center comments-border'>Comments are turned off.</div>
         </div>
     )
     
